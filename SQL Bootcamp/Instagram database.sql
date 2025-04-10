b47f541ece4e43620e6ade23e03b934566eb4c6c
Davvid01 <deberedawid@gmail.com>
2025-04-10 17:26:20 +0200

    instagram_data

---
+++ SQL Bootcamp/Instagram database.sql
@@ -0,0 +1,75 @@
+﻿create table users (
+id int identity(1,1) primary key,
+username varchar(255) unique not null,
+created_at datetime default getdate() --  now() w mysql, timestmap też w mysql -Techniczny binarny typ do śledzenia zmian w wierszach (alias rowversion) – nie data/czas!
+)
+
+insert into users (username) values
+('BlueTheCat'),('Charlie'),('Colte')
+
+
+
+create table photos (
+id int identity(1,1) primary key,
+user_id int not null,
+image_url varchar(255) not null,
+created_at datetime default getdate(),
+constraint fk_user_id_photos foreign key (user_id) references users(id)
+)
+alter table photos
+add constraint unique_url unique(image_url)
+
+insert into photos (image_url, user_id) values 
+('/dasda',1), ('/ddfgfgda',1), ('/ghbf4566',1)
+
+select *, username from photos
+join users on photos.user_id=users.id
+
+
+create table comments (
+id int identity(1,1) primary key,
+user_id int not null,
+photo_id int not null,
+text_message varchar(255) not null, 
+created_at datetime default getdate(),
+foreign key (user_id) references users(id),
+foreign key (photo_id) references photos(id)
+)
+
+sp_help comments
+
+create table likes (
+--id int identity(1,1) primary key, nie potrzebujemy, bo w gdy user likeuje kilka razy to wciaz bedzie naliczalo kolejne like'i
+-- likei nie przechowuja zadnej innej informacji wiec w innych tabelach nie bedziemy sie odwolywac do tej tabeli przez klucze or sth
+user_id int not null,
+photo_id int not null,
+created_at datetime default getdate(),
+constraint likes_user_id foreign key (user_id) references users(id),
+constraint likes_photo_id foreign key (photo_id) references photos(id),
+primary key(user_id, photo_id) -- musi byc unikalna kombinacja, unikamy dublowania likeow
+)
+
+create table tags (
+id int identity(1,1) primary key,
+tag_name varchar(255) unique,
+created_at datetime default getdate()
+)
+
+create table photo_tags (
+tag_id int not null,
+photo_id int not null,
+foreign key(tag_id) references tags(id),
+foreign key (photo_id) references photos(id),
+primary key(tag_id, photo_id) --nie powstanie kolumna id
+
+sp_help photo_tags
+
+create table follows (
+follower_id int not null,-- person who gave followe 
+followee_id int not null,-- person who got 
+created_at datetime default getdate(),
+foreign key (follower_id) references users(id),
+foreign key (followee_id) references users(id),
+primary key(follower_id, followee_id)
+)
+
--- SQL Bootcamp/windows functions.sql
+++ SQL Bootcamp/windows functions.sql
@@ -27,6 +27,7 @@ insert into employees (department, salary) values
 ('customer service', 55000);
 
 --WINDOWS FUNCTION wyswietla wszystkiego wiersze, ale wyswitela obok funkcje grupującą w zaleznosci co grupowalismy
+--czyli w wyswitelna kolumna odwoluje sie do grupowania/ danej grupy, ale roznica jest taka ze wyswietla się ja w nomrlanym selectcie
 select emp_no, salary, department, avg(salary) over() as 'general avg', min(salary) over(), max(salary) over() from employees
 group by department
 
@@ -49,8 +50,47 @@ select emp_no, department, salary, min(salary) over (partition by department ord
 sum(salary) over(partition by department order by salary desc) as rolling_dep_sal
 from employees
 
-select emp_no, department, salary, rank() over (partition by department order by salary desc) as by_dep,
-rank() over (order by salary desc) as overall
+select emp_no, department, salary, -- if values are the same -> consecutive number
+row_number() over (partition by department order by salary desc) as dept_row_number, -- if values are the same -> same number
+rank() over (partition by department order by salary desc) as by_dep,
+rank() over (order by salary desc) as overall, --1,2,3,4,4,6
+DENSE_RANK() over(order by salary desc) as overall_dense_rank -- if values are the same, same number. BUT subsequnt number is not skipped. 1,2,3,4,4,5
 from employees
 order by department, by_dep
 
+
+-- KWARTYLE
+
+select emp_no, department, salary, ntile(4) over(order by salary desc) as salary_quartile, -- jesli nie podamy partition by to znaczy że bedzie wzgledem kolumny w order by
+ntile(4) over(partition by department order by salary desc) as dept_salary_quartile
+--w departamencie ktos jest w 4 kwartyulu bo najnmniej zaraiba, ale overall w firmie już w drugim
+from employees
+
+-- FIRST VALUE
+
+select emp_no, department, salary, 
+FIRST_VALUE(emp_no) over(order by salary desc),
+--divide all rows into one window
+-- we have one massive window so for this window there is only 1 and first employee with highest salry
+FIRST_VALUE(emp_no) over(partition by department order by salary desc) as highest_paid_dep
+-- kazde okno to jeden department, sortujemy je, bierzemy pierwsza wartosc
+from employees
+
+--LEAD & LAG
+-- LAG shows preceding value
+-- LEAD shows subsequent
+
+
+select emp_no, department, salary, 
+lag(salary) over(order by salary desc),
+salary - lag(salary) over(order by salary desc) as salary_diff,
+salary - lead(salary) over(order by salary desc) as salary_diff_subseq
+--within one window
+from employees
+
+select emp_no, department, salary, 
+lag(salary) over(order by salary desc) as previous,
+salary - lag(salary) over(partition by department order by salary desc) as salary_diff,
+salary - lead(salary) over(order by salary desc) as salary_diff_subseq -- to cały czas wzgledem jednego duzego 'window'
+
+from employees
